1a0c8fc2d216fdc3e64eba30716adb62393f4f62
JSE <94772931+jeongseongeoop@users.noreply.github.com>
2022-07-28 20:52:28 +0900

    Initial commit

---
+++ Num.java
@@ -0,0 +1,9 @@
+public class Num{
+    public static void main(String[] args) {
+        short sVal = 10;
+        // int aVal = 100 ;
+        // long cVal = 100000;
+        byte bVal = 20;
+        System.out.println(sVal+bVal);
+    }
+}
