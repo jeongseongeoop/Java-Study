01429694679b1c81cb30e5f6a3cd61b082dd0aaf
JSE <94772931+jeongseongeoop@users.noreply.github.com>
2022-08-07 15:37:15 +0900

    chapter01
    변수와 자료형
    연습문제.1

---
+++ chapter01/example_1.java
@@ -0,0 +1,8 @@
+package chapter01;
+
+public class example_1 {
+    public static void main(String[] args){
+        System.out.println("연습문제 1. 내 이름 출력하기");
+        System.out.println("My name is SeongEop Jung");
+    }
+}
