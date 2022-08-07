9e4a431dcb8c2bfcbacc0f1d87d9126c99d8e7f6
JSE <94772931+jeongseongeoop@users.noreply.github.com>
2022-08-07 15:31:59 +0900

    220807_branche
    자바 예제 다시짜기

--- chapter01/First.java
+++
@@ -1,7 +0,0 @@
-package chapter01;
-
-public class First {
-    public static void main(String[] args) {
-        System.out.println("hello, world!");
-    }
-}
--- chapter01/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package chapter01;
-
-public class HelloWorld{
-    public static void main(String[] args) {
-        System.out.println("hello, world!");
-    }
-}
