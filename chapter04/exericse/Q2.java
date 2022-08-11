8c726428b7ec0e2999a3f8368a9c8acf4730924e
JSE <94772931+jeongseongeoop@users.noreply.github.com>
2022-08-11 22:27:36 +0900

    220811

--- chapter04/exericse/Q2.java
+++ chapter04/exericse/Q2.java
@@ -1,1 +1,16 @@
+package chapter04.exericse;
 
+
+public class Q2 {
+    public static void main(String[] args){
+        int num;
+        int sum;
+
+        for (num=2; num<=9; num++ ){
+            for(sum=1; sum<=9; sum++){
+            }
+            System.out.println(sum);
+            // System.out.println((sum+"x"+num+"="+sum*num));
+        }
+    }
+}
