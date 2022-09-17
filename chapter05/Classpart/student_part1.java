3d4d2d0010a6b5b13a58207dea18790472f69395
JSE <94772931+jeongseongeoop@users.noreply.github.com>
2022-09-17 14:39:01 +0900

    220917 class익히기
    멤버변수, 메서드 사용

---
+++ chapter05/Classpart/student_part1.java
@@ -0,0 +1,12 @@
+package chapter05.Classpart;
+
+public class student_part1 {
+    int StudentID;
+    String StudenName;
+    int grade;
+    String addres;
+
+    public void showStudentinfo(){
+        System.out.println(StudenName+","+addres); // 이름/주소 출력
+    }
+}
